1978d1f86d9c1be3845fb3f7ac97fb5ea4153e5b
Yunchuan Chen <chenych11@gmail.com>
2022-04-30 10:20:07 +0800

    fix: 测试和demo不能运行问题

--- test/load_test.cpp
+++ test/load_test.cpp
@@ -9,46 +9,57 @@
 
 using namespace cppjieba;
 
+const char* const DICT_PATH = "../dict/jieba.dict.utf8";
+const char* const HMM_PATH = "../dict/hmm_model.utf8";
+const char* const USER_DICT_PATH = "../dict/user.dict.utf8";
+const char* const IDF_PATH = "../dict/idf.utf8";
+const char* const STOP_WORD_PATH = "../dict/stop_words.utf8";
+
 void Cut(size_t times = 50) {
-  MixSegment seg("../dict/jieba.dict.utf8", "../dict/hmm_model.utf8");
-  vector<string> res;
-  string doc;
-  ifstream ifs("../test/testdata/weicheng.utf8");
-  assert(ifs);
-  doc << ifs;
-  long beginTime = clock();
-  for (size_t i = 0; i < times; i ++) {
-    printf("process [%3.0lf %%]\r", 100.0*(i+1)/times);
-    fflush(stdout);
-    res.clear();
-    seg.Cut(doc, res);
-  }
-  printf("\n");
-  long endTime = clock();
-  ColorPrintln(GREEN, "Cut: [%.3lf seconds]time consumed.", double(endTime - beginTime)/CLOCKS_PER_SEC);
+    DictTrie trie(DICT_PATH, USER_DICT_PATH);
+    HMMModel model(HMM_PATH);
+    MixSegment seg(&trie, &model);
+    vector<string> res;
+    string doc;
+    ifstream ifs("../test/testdata/weicheng.utf8");
+    assert(ifs);
+    doc << ifs;
+    long beginTime = clock();
+    for (size_t i = 0; i < times; i++) {
+        printf("process [%3.0lf %%]\r", 100.0 * (i + 1) / times);
+        fflush(stdout);
+        res.clear();
+        seg.CutToStr(doc, res, true);
+    }
+    printf("\n");
+    long endTime = clock();
+    ColorPrintln(GREEN, "Cut: [%.3lf seconds]time consumed.", double(endTime - beginTime) / CLOCKS_PER_SEC);
 }
 
 void Extract(size_t times = 400) {
-  KeywordExtractor Extractor("../dict/jieba.dict.utf8", "../dict/hmm_model.utf8", "../dict/idf.utf8", "../dict/stop_words.utf8");
-  vector<string> words;
-  string doc;
-  ifstream ifs("../test/testdata/review.100");
-  assert(ifs);
-  doc << ifs;
-  long beginTime = clock();
-  for (size_t i = 0; i < times; i ++) {
-    printf("process [%3.0lf %%]\r", 100.0*(i+1)/times);
-    fflush(stdout);
-    words.clear();
-    Extractor.Extract(doc, words, 5);
-  }
-  printf("\n");
-  long endTime = clock();
-  ColorPrintln(GREEN, "Extract: [%.3lf seconds]time consumed.", double(endTime - beginTime)/CLOCKS_PER_SEC);
+    DictTrie trie(DICT_PATH, USER_DICT_PATH);
+    HMMModel model(HMM_PATH);
+
+    KeywordExtractor Extractor(&trie, &model, IDF_PATH, STOP_WORD_PATH);
+    vector<string> words;
+    string doc;
+    ifstream ifs("../test/testdata/review.100");
+    assert(ifs);
+    doc << ifs;
+    long beginTime = clock();
+    for (size_t i = 0; i < times; i++) {
+        printf("process [%3.0lf %%]\r", 100.0 * (i + 1) / times);
+        fflush(stdout);
+        words.clear();
+        Extractor.Extract(doc, words, 5);
+    }
+    printf("\n");
+    long endTime = clock();
+    ColorPrintln(GREEN, "Extract: [%.3lf seconds]time consumed.", double(endTime - beginTime) / CLOCKS_PER_SEC);
 }
 
-int main(int argc, char ** argv) {
-  Cut();
-  Extract();
-  return EXIT_SUCCESS;
+int main(int argc, char **argv) {
+    Cut();
+    Extract();
+    return EXIT_SUCCESS;
 }
